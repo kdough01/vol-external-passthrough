6fba813d2bd2e7efbc411c6528b9227e98bbcf2b
Kevin Dougherty <kdougherty42301@gmail.com>
2026-06-04 13:35:27 -0500

    compressors

--- tests/test_cpu_hurricane.c
+++ tests/test_cpu_hurricane.c
@@ -69,7 +69,8 @@ int main() {
 
     /* --- GPU compressor (nvcomp_lz4 or whatever your CUDA build exposes) --- */
     /* Adjust the compressor name to match your gpu_compression registration  */
-    hid_t dcpl = make_dcpl("nvcomp_lz4", NULL);
+    hid_t dcpl = make_dcpl("sz3",
+    "{\"sz3:error_bound_mode_str\": \"abs\", \"sz3:abs_error_bound\": 1e-3}");
     hid_t dset = H5Dcreate2(file_id, "Pf48_gpu_lz4", H5T_NATIVE_FLOAT, space_id,
                              H5P_DEFAULT, dcpl, H5P_DEFAULT);
     H5Dwrite(dset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, field);
