9f48d32a5df76c0ee822e8369563a2cb50efbdba
Kevin Dougherty <kdougherty42301@gmail.com>
2026-06-16 12:30:04 -0500

    compression allocation

--- gpu_transform.cu
+++ gpu_transform.cu
@@ -101,6 +101,8 @@ H5VL_pass_through_ext_gpu_transfer_compress(gpu_vol_dataset_t* ds_ctx, const voi
                "cudaMemcpyAsync host->device failed");
 
     d_input = pressio_data_new_nonowning(in_dtype, gpu->d_in, in_ndims, in_dims);
+    pressio_data_set_domain(d_input, "cudamalloc"); 
+    
     d_output = pressio_data_new_empty(pressio_byte_dtype, 0, NULL);
 
     {
@@ -222,11 +224,13 @@ H5VL_pass_through_ext_gpu_transfer_decompress(gpu_vol_dataset_t* ds_ctx, const v
                "cudaMemcpy host->device for compressed data failed");
 
     d_input = pressio_data_new_move(pressio_byte_dtype, d_comp, 1, comp_dims, cuda_deleter, NULL);
+    pressio_data_set_domain(d_input, "cudamalloc"); 
     d_comp  = NULL;
 
     CUDA_CHECK(cudaMalloc(&d_out_buf, output_nbytes),
                "cudaMalloc for decompress output buffer failed");
     d_output = pressio_data_new_nonowning(out_dtype, d_out_buf, out_ndims, out_dims);
+    pressio_data_set_domain(d_output, "cudamalloc");
 
     {
         const char* skey = gpu_stream_key(ctx->compressor_id);
