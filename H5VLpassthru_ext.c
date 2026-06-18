5189c05382f9cce0413c3872d2d26805c2d0ed66
Kevin Dougherty <kdougherty42301@gmail.com>
2026-06-18 10:32:44 -0500

    time tests

--- H5VLpassthru_ext.c
+++ H5VLpassthru_ext.c
@@ -1659,25 +1659,6 @@ H5VL_pass_through_ext_dataset_create(void *obj,
                 H5VLattr_close(attr_comp, o->under_vol_id, dxpl_id, NULL);
             }
 
-            pressio_options *opts = pressio_compressor_get_options(ctx->compressor);
-            if (opts) {
-                char *json = pressio_options_as_json(opts);
-                if (json) {
-                    hid_t atype = H5Tcopy(H5T_C_S1);
-                    H5Tset_size(atype, strlen(json) + 1);
-                    H5Tset_strpad(atype, H5T_STR_NULLTERM);
-                    hid_t aspace = H5Screate(H5S_SCALAR);
-                    hid_t attr   = H5Acreate2(dset_id, "_VOL_OPTIONS_JSON",
-                                            atype, aspace, H5P_DEFAULT, H5P_DEFAULT);
-                    H5Awrite(attr, atype, json);
-                    H5Aclose(attr);
-                    H5Sclose(aspace);
-                    H5Tclose(atype);
-                    free(json);
-                }
-                pressio_options_free(opts);
-            }
-
             H5Tclose(str_type);
             H5Pclose(acpl_id);
             H5Pclose(aapl_id);
@@ -1706,6 +1687,32 @@ H5VL_pass_through_ext_dataset_create(void *obj,
                 H5VL_pass_through_ext_free_obj(dset);
                 free(h5dims);
                 return NULL;
+            }
+
+            if (ds_ctx && ds_ctx->comp_ctx && ds_ctx->comp_ctx->compressor) {
+                struct pressio_options *opts =
+                    pressio_compressor_get_options(ds_ctx->comp_ctx->compressor);
+                if (opts) {
+                    char *json = pressio_options_to_json(opts);
+                    if (json) {
+                        hid_t json_str_type = H5Tcopy(H5T_C_S1);
+                        H5Tset_size(json_str_type, strlen(json) + 1);
+                        H5Tset_strpad(json_str_type, H5T_STR_NULLTERM);
+                        hid_t json_space = H5Screate(H5S_SCALAR);
+                        void *attr_json = H5VLattr_create(under, &attr_loc, o->under_vol_id,
+                            "_VOL_OPTIONS_JSON", json_str_type, json_space,
+                            H5P_DEFAULT, H5P_DEFAULT, dxpl_id, NULL);
+                        if (attr_json) {
+                            H5VLattr_write(attr_json, o->under_vol_id,
+                                        json_str_type, json, dxpl_id, NULL);
+                            H5VLattr_close(attr_json, o->under_vol_id, dxpl_id, NULL);
+                        }
+                        H5Sclose(json_space);
+                        H5Tclose(json_str_type);
+                        free(json);
+                    }
+                    pressio_options_free(opts);
+                }
             }
 
             H5Sclose(underlying_space_id);
@@ -1824,14 +1831,19 @@ H5VL_pass_through_ext_dataset_open(void *obj,
         H5Pclose(aapl_id);
 
         char *json_buf = NULL;
-        if (H5Aexists(dset_id, "_VOL_OPTIONS_JSON") > 0) {
-            hid_t attr   = H5Aopen(dset_id, "_VOL_OPTIONS_JSON", H5P_DEFAULT);
-            hid_t atype  = H5Aget_type(attr);
-            size_t jlen  = H5Tget_size(atype);
-            json_buf     = malloc(jlen);
-            H5Aread(attr, atype, json_buf);
-            H5Tclose(atype);
-            H5Aclose(attr);
+        void *attr_json = H5VLattr_open(under, &attr_loc, o->under_vol_id,
+            "_VOL_OPTIONS_JSON", H5P_DEFAULT, dxpl_id, NULL);
+        if (attr_json) {
+            H5VL_attr_get_args_t ag;
+            ag.op_type = H5VL_ATTR_GET_TYPE;
+            ag.args.get_type.type_id = H5I_INVALID_HID;
+            H5VLattr_get(attr_json, o->under_vol_id, &ag, dxpl_id, NULL);
+            hid_t json_type = ag.args.get_type.type_id;
+            size_t jlen = H5Tget_size(json_type);
+            json_buf = malloc(jlen);
+            H5VLattr_read(attr_json, o->under_vol_id, json_type, json_buf, dxpl_id, NULL);
+            H5Tclose(json_type);
+            H5VLattr_close(attr_json, o->under_vol_id, dxpl_id, NULL);
         }
 
         dset->custom_data = gpu_vol_dataset_wrap(under, recovered_rank, recovered_dims,
@@ -1839,21 +1851,21 @@ H5VL_pass_through_ext_dataset_open(void *obj,
                                           real_dcpl_id, o->under_vol_id, file_ctx,
                                           recovered_comp);
 
-        if (json_buf && new_dset->comp_ctx && new_dset->comp_ctx->compressor) {
-            pressio_options *opts = pressio_options_new_from_json(json_buf);
+        gpu_vol_dataset_t *ds_ctx = (gpu_vol_dataset_t*)dset->custom_data;
+        if (json_buf && ds_ctx && ds_ctx->comp_ctx && ds_ctx->comp_ctx->compressor) {
+            struct pressio_options *opts = pressio_options_new_json(json_buf);
             if (opts) {
-                pressio_compressor_set_options(new_dset->comp_ctx->compressor, opts);
+                pressio_compressor_set_options(ds_ctx->comp_ctx->compressor, opts);
                 pressio_options_free(opts);
             }
             free(json_buf);
             json_buf = NULL;
 
-            /* re-apply the CUDA stream */
-            const char *stream_key = gpu_stream_key(new_dset->comp_ctx->compressor_id);
-            if (stream_key && new_dset->gpu_ctx) {
-                pressio_options *sopts = pressio_options_new();
-                pressio_options_set_userptr(sopts, stream_key, new_dset->gpu_ctx->stream);
-                pressio_compressor_set_options(new_dset->comp_ctx->compressor, sopts);
+            const char *stream_key = gpu_stream_key(ds_ctx->comp_ctx->compressor_id);
+            if (stream_key && ds_ctx->gpu_ctx) {
+                struct pressio_options *sopts = pressio_options_new();
+                pressio_options_set_userptr(sopts, stream_key, ds_ctx->gpu_ctx->stream);
+                pressio_compressor_set_options(ds_ctx->comp_ctx->compressor, sopts);
                 pressio_options_free(sopts);
             }
         }
