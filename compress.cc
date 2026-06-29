acd9c65f8f271556144c48777995c99188f68dd4
Kevin Dougherty <kdougherty42301@gmail.com>
2026-06-29 12:50:57 -0500

    debug repack

--- compress.cc
+++ compress.cc
@@ -139,6 +139,8 @@ H5VL_pass_through_ext_transfer_compress(compression_ctx *ctx, const void *data, 
                 (void *)output, pressio_data_get_bytes(output),
                 pressio_data_get_capacity_in_bytes(output),
                 pressio_data_domain_id(output));
+        const char* comp_id = pressio_compressor_get_id(ctx->compressor);
+        fprintf(stderr, "DEBUG: Current compressor ID is: %s\n", comp_id);
         fflush(stderr);
     }
 #endif
