7d705c1373df59a1f3bc1af991b9088ec5fd4ee6
Kevin Dougherty <kdougherty42301@gmail.com>
2026-07-17 12:58:14 -0500

    test

---
+++ tests/dump_cuszp_opts.cc
@@ -0,0 +1,24 @@
+#include <libpressio.h>
+#include <cstdio>
+#include <cstdlib>
+
+int main(void) {
+    struct pressio* lib = pressio_instance();
+
+    struct pressio_compressor* c = pressio_get_compressor(lib, "cuszp");
+    if (!c) {
+        std::fprintf(stderr, "cuszp not available: %s\n", pressio_error_msg(lib));
+        pressio_release(lib);
+        return 1;
+    }
+
+    struct pressio_options* o = pressio_compressor_get_options(c);
+    char* s = pressio_options_to_string(o);
+    std::printf("%s\n", s ? s : "(null)");
+
+    std::free(s);
+    pressio_options_free(o);
+    pressio_compressor_release(c);
+    pressio_release(lib);
+    return 0;
+}
