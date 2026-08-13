280a2636520898be2a9277519c7c48c73b150479
Kevin Dougherty <kdougherty@gpulogin2.lcrc.anl.gov>
2026-08-13 15:55:17 -0500

    device tests

---
+++ build_g2/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kdougherty/hdf5_projs/vol-external-passthrough
+# Build directory: /home/kdougherty/hdf5_projs/vol-external-passthrough/build_g2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ build_g2/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kdougherty/hdf5_projs/vol-external-passthrough/tests
+# Build directory: /home/kdougherty/hdf5_projs/vol-external-passthrough/build_g2/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
