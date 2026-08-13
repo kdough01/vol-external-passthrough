702755ff65b8fb77fe54b20cfa54693fb8510ed8
Kevin Dougherty <kdougherty42301@gmail.com>
2026-08-13 10:46:47 -0700

    cleanup

--- build_zfpfilter/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kdougherty/hdf5_projs/vol-external-passthrough
-# Build directory: /home/kdougherty/hdf5_projs/vol-external-passthrough/build_zfpfilter
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
-subdirs("examples")
