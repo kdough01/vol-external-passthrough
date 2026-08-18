accdc40fb80d1ff694d5b6c584da5cb1c7865ac1
Kevin Dougherty <kdougherty42301@gmail.com>
2026-08-18 11:44:23 -0700

    larger progressive dataset

--- tests/run_progressive.sh
+++ tests/run_progressive.sh
@@ -85,7 +85,7 @@ mkdir -p "$H5DIR" || { echo "cannot create $H5DIR"; exit 1; }
 
 PROG_DSET="${PROG_DSET:-s3d}"
 PROG_SEL="${PROG_SEL:-s3d}"
-PROG_COMPS="${PROG_COMPS:- sperr_pwe1e6}"
+PROG_COMPS="${PROG_COMPS:- sperr_pwe1e3}"
 PROG_CHUNKING="${PROG_CHUNKING:-}"
 PROG_CHUNK_N="${PROG_CHUNK_N:-}"
 PCTS="${PROG_PCTS:-5 10 25 50 75 100}"
